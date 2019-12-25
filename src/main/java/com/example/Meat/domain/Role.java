a4c34815f978f85afb291c7fb964703b57621783
Zver <tsydenov.zanabazar@gmail.com>
2019-12-25 17:17:35 +0700

    Add admin role and check rights by 'hasAuthority'

--- src/main/java/com/example/Meat/domain/Role.java
+++ src/main/java/com/example/Meat/domain/Role.java
@@ -3,7 +3,8 @@ package com.example.Meat.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER,
+    ADMIN;
 
     @Override
     public String getAuthority() {
