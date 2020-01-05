44735aeebaaba5c975b210f66d523d95bb906d77
Zver <tsydenov.zanabazar@gmail.com>
2020-01-05 18:08:08 +0700

    Add small changes

--- src/main/java/com/example/Meat/controller/MessageController.java
+++ src/main/java/com/example/Meat/controller/MessageController.java
@@ -65,6 +65,24 @@ public class MessageController {
         return "main";
     }
 
+
+    private void saveFile(@Valid Message message, @RequestParam("file") MultipartFile file) throws IOException {
+        if (file != null && !Objects.requireNonNull(file.getOriginalFilename()).isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+    }
+
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
@@ -90,69 +108,7 @@ public class MessageController {
         return "redirect:/main";
     }
 
-    private void saveFile(@Valid Message message, @RequestParam("file") MultipartFile file) throws IOException {
-        if (file != null && !Objects.requireNonNull(file.getOriginalFilename()).isEmpty()) {
-            File uploadDir = new File(uploadPath);
 
-            if (!uploadDir.exists()) {
-                uploadDir.mkdir();
-            }
-
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-            file.transferTo(new File(uploadPath + "/" + resultFilename));
-
-            message.setFilename(resultFilename);
-        }
-    }
-
-    @GetMapping("/user-messages/{author}")
-    public String userMessages(
-            @AuthenticationPrincipal User currentUser,
-            @PathVariable User author,
-            Model model,
-            @RequestParam(required = false) Message message,
-            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
-    ) {
-        Page<MessageDto> page = messageService.messageListForUser(pageable, currentUser, author);
-
-        model.addAttribute("userChannel", author);
-        model.addAttribute("subscriptionsCount", author.getSubscriptions().size());
-        model.addAttribute("subscribersCount", author.getSubscribers().size());
-        model.addAttribute("isSubscriber", author.getSubscribers().contains(currentUser));
-        model.addAttribute("page", page);
-        model.addAttribute("message", message);
-        model.addAttribute("isCurrentUser", currentUser.equals(author));
-        model.addAttribute("url", "/user-messages/" + author.getId());
-
-        return "userMessages";
-    }
-
-    @PostMapping("/user-messages/{user}")
-    public String updateMessage(
-            @AuthenticationPrincipal User currentUser,
-            @PathVariable Long user,
-            @RequestParam("id") Message message,
-            @RequestParam("text") String text,
-            @RequestParam("tag") String tag,
-            @RequestParam("file") MultipartFile file
-    ) throws IOException {
-        if (message.getAuthor().equals(currentUser)) {
-            if (!StringUtils.isEmpty(text)) {
-                message.setText(text);
-            }
-
-            if (!StringUtils.isEmpty(tag)) {
-                message.setTag(tag);
-            }
-            saveFile(message, file);
-
-            messageRepo.save(message);
-        }
-
-        return "redirect:/user-messages/" + user;
-    }
 
     @GetMapping("/messages/{message}/like")
     public String like(
---
+++ src/main/java/com/example/Meat/controller/UserMessageController.java
@@ -0,0 +1,104 @@
+package com.example.Meat.controller;
+
+import com.example.Meat.domain.Message;
+import com.example.Meat.domain.User;
+import com.example.Meat.domain.dto.MessageDto;
+import com.example.Meat.repos.MessageRepo;
+import com.example.Meat.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/user-messages")
+public class UserMessageController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @Autowired
+    private MessageService messageService;
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+
+
+    @GetMapping("{author}")
+    public String userMessages(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User author,
+            Model model,
+            @RequestParam(required = false) Message message,
+            @PageableDefault(sort = {"id"}, direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        Page<MessageDto> page = messageService.messageListForUser(pageable, currentUser, author);
+
+        model.addAttribute("userChannel", author);
+        model.addAttribute("subscriptionsCount", author.getSubscriptions().size());
+        model.addAttribute("subscribersCount", author.getSubscribers().size());
+        model.addAttribute("isSubscriber", author.getSubscribers().contains(currentUser));
+        model.addAttribute("page", page);
+        model.addAttribute("message", message);
+        model.addAttribute("isCurrentUser", currentUser.equals(author));
+        model.addAttribute("url", "/user-messages/" + author.getId());
+
+        return "userMessages";
+    }
+
+    @PostMapping("{user}")
+    public String updateMessage(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable Long user,
+            @RequestParam("id") Message message,
+            @RequestParam("text") String text,
+            @RequestParam("tag") String tag,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+        if (message.getAuthor().equals(currentUser)) {
+            if (!StringUtils.isEmpty(text)) {
+                message.setText(text);
+            }
+
+            if (!StringUtils.isEmpty(tag)) {
+                message.setTag(tag);
+            }
+            saveFile(message, file);
+
+            messageRepo.save(message);
+        }
+
+        return "redirect:/user-messages/" + user;
+    }
+
+    private void saveFile(Message message, MultipartFile file) throws IOException {
+        if (file != null && !Objects.requireNonNull(file.getOriginalFilename()).isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+    }
+}
